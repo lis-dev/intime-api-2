4a3c968e018c16f51cedc7c60ddde5926cdc27c0
lis-ua <mathfak@gmail.com>
2014-10-20 12:59:47 +0300

    Init

---
+++ src/IntimeApi.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Класс для работы с API службы доставки Интайм (Украина)
+ * 
+ * @author lis-dev
+ * @link http://www.intime.ua/API2.0/API_2_0.pdf
+ * @version 0.01
+ */
+class IntimeApi {
+	
+}
+?>
